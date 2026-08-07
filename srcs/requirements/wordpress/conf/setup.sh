50b00f1e848664b9bb047abe397d4c60fdabdefc
motelti <mteltipr03@gmail.com>
2026-08-07 19:59:30 +0100

    begin implementing setup.sh for wordpress

---
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+
+    wp core download --allow-root
+
+    wp config create --dbname="${db_name}" --dbuser="${db_user}" --dbpass="${db_user_pass}" --dbhost="mariadb" --allow-root
+
+    wp core install --url="${DOMAIN_NAME}" --title="Inception" --admin_user="${WP_ADMIN}" --admin_password="${WP_ADMIN_PASS}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
+
+    wp user create "${WP_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}" --role=author --allow-root
+fi
+
+exec php-fpm8.2 -F
